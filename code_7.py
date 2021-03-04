a84f7c9749b0c1ed3fa43e6b846e62cdba2bf0ac
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-03-04 18:00:10 +0530

    Add files via upload

--- code_7.py
+++ code_7.py
@@ -1,5 +1,8 @@
+from datetime import datetime as dt
+
 helloIntent = ["hi","hey","hello","hi there"]
 dateIntent = ["date","tell me date","what's the date"]
+timeIntent = ["time","tell me time","what's the time"]
 
 chat = True
 while chat:
@@ -7,8 +10,15 @@ while chat:
     msg = msg.lower()
     if msg in helloIntent:
         print("Hello User")
+
     elif msg in dateIntent:
-        pass
+        current_date = dt.now().date()
+        print(current_date.strftime('%d/%m/%y, %a'))
+
+    elif msg in timeIntent:
+        current_time = dt.now().time()
+        print(current_time.strftime("%H:%M:%S, %p"))
+        
     elif msg == "bye":
         print("Bye User")
         chat = False
